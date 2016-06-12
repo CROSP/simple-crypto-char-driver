bd75d1e813bb4890a07cf136c84a1be728b83f8e
CROSP <crospdevel@gmail.com>
2016-06-12 16:31:35 +0300

    Add Makefile

--- module_test.c
+++ module_test.c
@@ -4,7 +4,6 @@
  * @date    12 June 2016
  * @version 1.0
  * @brief  Text source code for module
- * @see http://www.derekmolloy.ie/ for a full description and follow-up descriptions.
 */
 
 #include<stdio.h>
@@ -21,15 +20,15 @@ int main(){
    int ret, fd;
    char str_to_write[BUFFER_LENGTH];
    printf("Starting device test code example...\n");
-   fd = open(DEVICE_NAME, O_RDWR);             // Open the device with read/write access
+   fd = open(DEVICE_NAME, O_RDWR);
    if (fd < 0){
       perror("Failed to open the device...");
       return errno;
    }
    printf("Type in a short string to send to the kernel module:\n");
-   scanf("%[^\n]%*c", str_to_write);                // Read in a string (with spaces)
+   scanf("%[^\n]%*c", str_to_write);
    printf("Writing message to the device [%s].\n", str_to_write);
-   ret = write(fd, str_to_write, strlen(str_to_write)); // Send the string to the LKM
+   ret = write(fd, str_to_write, strlen(str_to_write));
    if (ret < 0){
       perror("Failed to write the message to the device.");
       return errno;
@@ -39,7 +38,7 @@ int main(){
    getchar();
 
    printf("Reading from the device...\n");
-   ret = read(fd, receive, BUFFER_LENGTH);        // Read the response from the LKM
+   ret = read(fd, receive, BUFFER_LENGTH);
    if (ret < 0){
       perror("Failed to read the message from the device.");
       return errno;
